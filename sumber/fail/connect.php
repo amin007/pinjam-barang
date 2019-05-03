1928a0fc86fe0a3fd9e670857b095b887d6fb43b
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-03 10:24:36 +0800

    buang semua fail

--- sumber/fail/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","","borrow");
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
- 
-?>
--- sumber/fail/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-	session_start();
-	session_unset($_SESSION['admin_id']);
-	header('location: index.php');
