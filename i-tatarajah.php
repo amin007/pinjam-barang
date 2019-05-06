8a15a8fe8df720f7d79eb191f7c60437755b2673
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-06 15:12:01 +0800

    tambah i-tatarajah.pho

---
+++ i-tatarajah.php
@@ -0,0 +1,10 @@
+<?php
+# 4 folder utama
+define('KAWAL', 'Aplikasi/Kelas/Utama/Kawal');
+define('TANYA', 'Aplikasi/Kelas/Utama/Tanya');
+define('PAPAR', 'Aplikasi/Fail/Papar');
+define('KITAB', 'Aplikasi/Kelas/Kitab');
+define('FUNGSI', 'Aplikasi/Fungsi');
+
+# Fungsi Global
+require FUNGSI . '/Fungsi.php';
