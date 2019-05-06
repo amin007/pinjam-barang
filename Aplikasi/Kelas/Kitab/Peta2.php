6b9f3a1039f78b495d00aef8ed5a56fb289cfa3a
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-06 19:01:20 +0800

    buang balik koding asal

--- Aplikasi/Kelas/Kitab/Peta2.php
+++ Aplikasi/Kelas/Kitab/Peta2.php
@@ -11,68 +11,12 @@ class Peta2
 {
 ####################################################################################################
 #---------------------------------------------------------------------------------------------------
-	public function parseURL()
-	{
-		//$this->semakPembolehubah($_GET,'_GET');
-		if( isset($_GET['url']) )
-		{
-			$url = rtrim($_GET['url'], '/');
-			$url = filter_var($url, FILTER_SANITIZE_URL);
-			$url = explode('/', $url);
-			return $url;
-		}
-	}
 #---------------------------------------------------------------------------------------------------
 	public function __construct()
 	{
 		//echo '<hr> Nama class : ' . __METHOD__ . '<hr>';
-		# 1. capai fungsi $this->parseURL() dan masukkan dalam $url
-		$url = $this->parseURL();
-		list($url,$Url) = $this->semakURL($url);
-		//$this->debugData($url,$Url);#semak untuk masa depan
-
-		# 2. cari controller => kawal
-		$url = $this->semakKawal($url,$Url);
-		//$this->semakPembolehubah($url,'url selepas class');
-
-		# 3. cari method => fungsi
-		$url = $this->semakMethod($url);
-		//$this->semakPembolehubah($url,'url selepas method');
-
 	}
 #---------------------------------------------------------------------------------------------------
-	function semakKawal($url,$Url)
-	{
-		if( file_exists(KAWAL . '/' . $url[0] . '.php') )
-		{
-			$this->kawal = $Url[0];
-			//echo 'lokasi fail:' . KAWAL . '/' . $url[0] . '.php<hr>';
-			//echo 'nama class:' . $this->kawal . '<hr>';
-			require_once KAWAL . '/' . $url[0] . '.php';
-			unset($url[0]);
-		}
-		$this->kawal = new $this->kawal;# nilai default adalah index
-
-		return $url;
-	}
-#---------------------------------------------------------------------------------------------------
-	function semakMethod($url)
-	{
-		# jika $url[1] tak disetkan, bagi $method='index'
-		$method = (isset($url[1])) ? $url[1] : 'index';
-
-		if( isset($method) )
-		{
-			if( method_exists($this->kawal, $method) )
-			{
-				$this->method = $method;# nilai default adalah index
-				//echo 'nama method:' . $method . '<hr>';
-				unset($url[1]);
-			}
-		}
-
-		return $url;
-	}
 #---------------------------------------------------------------------------------------------------
 ####################################################################################################
 }
