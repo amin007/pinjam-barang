aad2baf8e45155e98fe0b517075a8989825ccf9a
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-06 10:18:26 +0800

    tukar pautan baru

--- sumber/fail/asal/connect.php
+++ sumber/fail/asal/connect.php
@@ -1,6 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","borrow");
+//$conn = mysqli_connect("localhost","root","","borrow");
+$conn = mysqli_connect("localhost","akar","akar","mvc_pinjambarang");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
