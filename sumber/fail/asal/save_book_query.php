99f2b6a60e23446f73a328fc8a62a970453bb989
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-05-06 10:15:34 +0800

    menyusun sql semula

--- sumber/fail/asal/save_book_query.php
+++ sumber/fail/asal/save_book_query.php
@@ -7,12 +7,17 @@
 		$book_author = $_POST['book_author'];
 		$date_publish = $_POST['date_publish'];
 		$qty = $_POST['qty'];
-		$conn->query("INSERT INTO `book` VALUES('', '$book_title', '$book_desc', '$book_category', '$book_author', '$date_publish', '$qty')") or die(mysqli_error());
+		$sql = "INSERT INTO `book`
+		VALUES('', '$book_title', '$book_desc', '$book_category', '$book_author', '$date_publish', '$qty')";
+		echo "<pre>$sql</pre>";
+		/*$conn->query("INSERT INTO `book`
+		VALUES('', '$book_title', '$book_desc', '$book_category', '$book_author', '$date_publish', '$qty')")
+		or die(mysqli_error());
 		
 		echo'
 			<script type = "text/javascript">
 				alert("Successfully saved data");
 				window.location = "book.php";
 			</script>
-		';
+		';*/
 	}
